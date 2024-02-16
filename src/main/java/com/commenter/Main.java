1f18fba9eefb71a9d639ec9a093ecdb58e9b30a4
joshuagatizz <anggapramono01@gmail.com>
2024-02-16 16:09:14 +0700

    Deleted sample handlers

--- src/main/java/com/commenter/Main.java
+++ src/main/java/com/commenter/Main.java
@@ -6,13 +6,9 @@ import ratpack.core.server.RatpackServer;
 public class Main {
   public static void main(String[] args) throws Exception {
     RatpackServer.start(server -> server
-        .serverConfig(config-> {
-          config.port(8080);
-        })
+        .serverConfig(config -> config.port(8080))
         .handlers(chain -> chain
-            .get(ctx -> ctx.render("Hello World!"))
-            .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))
-            .prefix("api", apiPath -> apiPath
+            .prefix("api", api -> api
                 .prefix("post", postPath -> postPath
                     .get("", new PostHandler())
                 )
