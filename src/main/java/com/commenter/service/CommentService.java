04edf597c86d9775f3fd6eebf5ec74f46d63b1e7
joshuagatizz <anggapramono01@gmail.com>
2024-02-20 16:27:51 +0700

    Add validation to comment apis

--- src/main/java/com/commenter/service/CommentService.java
+++ src/main/java/com/commenter/service/CommentService.java
@@ -2,8 +2,11 @@ package com.commenter.service;
 
 import com.commenter.model.Comment;
 import com.commenter.model.CreateEditCommentRequest;
+import com.commenter.model.ResponseVO;
 import com.google.inject.Inject;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -11,6 +14,8 @@ public class CommentService {
 
   private static final String GET_ALL_COMMENTS_BY_POST_ID_QUERY =
       "SELECT * FROM comments WHERE post_id = ?";
+  private static final String GET_COMMENT_BY_ID_AND_USER_QUERY =
+      "SELECT * FROM comments WHERE id = ? AND \"user\" = ?";
   private static final String CREATE_NEW_COMMENT_QUERY =
       "INSERT INTO comments (\"user\", content, post_id) VALUES (?, ?, ?) RETURNING id, \"user\", content, post_id";
   private static final String UPDATE_COMMENT_BY_ID_QUERY =
@@ -18,42 +23,96 @@ public class CommentService {
   private static final String DELETE_COMMENT_BY_ID_QUERY =
       "DELETE FROM comments WHERE id = ?";
   private final DatabaseService databaseService;
+  private final PostService postService;
 
   @Inject
-  public CommentService(DatabaseService databaseService) {
+  public CommentService(PostService postService, DatabaseService databaseService) {
+    this.postService = postService;
     this.databaseService = databaseService;
   }
 
-  public List<Comment> getAllCommentsByPostId(int postId) {
+  public ResponseVO<List<Comment>> getAllCommentsByPostId(int postId) {
+    if (postService.getPostById(postId).getData() == null) {
+      return ResponseVO.<List<Comment>>builder()
+          .errors(Collections.singletonList("Post data not found"))
+          .build();
+    }
     try {
-      return databaseService.executeQuery(GET_ALL_COMMENTS_BY_POST_ID_QUERY, postId)
+      List<Comment> comments =  databaseService.executeQuery(GET_ALL_COMMENTS_BY_POST_ID_QUERY, postId)
           .stream()
           .map(this::toComment)
           .toList();
+
+      return ResponseVO.<List<Comment>>builder().data(comments).build();
     } catch (Exception e) {
       throw new RuntimeException("Failed to get all comments by postId", e);
     }
   }
 
-  public Comment createNewComment(int postId, CreateEditCommentRequest request) {
-    return databaseService.executeQuery(
+  public ResponseVO<Comment> createNewComment(int postId, CreateEditCommentRequest request) {
+    List<String> errors = validateRequest(request);
+
+    if (postService.getPostById(postId).getData() == null) {
+      errors.add("Post data not found");
+    }
+
+    if (!errors.isEmpty()) {
+      return ResponseVO.<Comment>builder()
+          .errors(errors)
+          .build();
+    }
+    Map<String, Object> result = databaseService.executeQuery(
             CREATE_NEW_COMMENT_QUERY,
             request.getUser(), request.getContent(), postId)
         .stream()
         .findFirst()
-        .map(this::toComment)
-        .orElseThrow(() -> new RuntimeException("Failed to create a new comment"));
+        .orElse(null);
+    if (result != null) {
+      postService.modifyPostCommentById(postId, Boolean.TRUE);
+      return toCommentResponseVO(result);
+    } else {
+      throw new RuntimeException("Failed to create a new comment");
+    }
   }
 
-  public boolean editCommentById(int commentId, CreateEditCommentRequest request) {
-    int affectedRows = databaseService.executeUpdate(
-        UPDATE_COMMENT_BY_ID_QUERY, request.getContent(), commentId);
-    return affectedRows > 0;
+  public ResponseVO<Boolean> editCommentById(int commentId, CreateEditCommentRequest request) {
+    List<String> errors = validateRequest(request);
+
+    if (!errors.isEmpty()) {
+      return ResponseVO.<Boolean>builder()
+          .errors(errors)
+          .build();
+    }
+    try {
+      if (databaseService.executeQuery(GET_COMMENT_BY_ID_AND_USER_QUERY, commentId, request.getUser()).isEmpty()) {
+        return ResponseVO.<Boolean>builder().errors(Collections.singletonList("Comment data not found")).build();
+      }
+      databaseService.executeUpdate(UPDATE_COMMENT_BY_ID_QUERY, request.getContent(), commentId);
+      return ResponseVO.<Boolean>builder().data(Boolean.TRUE).build();
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to edit comment");
+    }
   }
 
-  public Object deleteCommentById(int commentId) {
-    int affectedRows = databaseService.executeUpdate(DELETE_COMMENT_BY_ID_QUERY, commentId);
-    return affectedRows > 0;
+  public ResponseVO<Boolean> deleteCommentById(int commentId, int postId, String user) {
+    if (user == null) {
+      return ResponseVO.<Boolean>builder().errors(Collections.singletonList("The user field cannot be empty")).build();
+    }
+    try {
+      List<Map<String, Object>> commentResult =
+          databaseService.executeQuery(GET_COMMENT_BY_ID_AND_USER_QUERY, commentId, user);
+      if (commentResult.isEmpty()) {
+        return ResponseVO.<Boolean>builder().errors(Collections.singletonList("Comment data not found")).build();
+      }
+      if (toComment(commentResult.getFirst()).getPostId() != postId) {
+        return ResponseVO.<Boolean>builder().errors(Collections.singletonList("Post id invalid")).build();
+      }
+      databaseService.executeUpdate(DELETE_COMMENT_BY_ID_QUERY, commentId);
+      postService.modifyPostCommentById(postId ,Boolean.FALSE);
+      return ResponseVO.<Boolean>builder().data(Boolean.TRUE).build();
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to delete comment");
+    }
   }
 
   private Comment toComment(Map<String, Object> mp) {
@@ -63,5 +122,20 @@ public class CommentService {
         .content((String) mp.get("content"))
         .postId((Integer) mp.get("post_id"))
         .build();
+  }
+
+  private ResponseVO<Comment> toCommentResponseVO(Map<String, Object> mp) {
+    return ResponseVO.<Comment>builder().data(toComment(mp)).build();
+  }
+
+  private List<String> validateRequest(CreateEditCommentRequest request) {
+    List<String> errors = new ArrayList<>();
+    if (request.getUser() == null || request.getUser().isEmpty()) {
+      errors.add("The user field cannot be empty");
+    }
+    if (request.getContent() == null || request.getContent().isEmpty()) {
+      errors.add("The content field cannot be empty");
+    }
+    return errors;
   }
 }
--- src/main/java/com/commenter/service/PostService.java
+++ src/main/java/com/commenter/service/PostService.java
@@ -14,12 +14,18 @@ public class PostService {
 
   private static final String GET_ALL_POST_QUERY =
       "SELECT * FROM posts";
-  private static final String GET_POST_BY_ID_AND_USER =
+  private static final String GET_POST_BY_ID_QUERY =
+      "SELECT * FROM posts WHERE id = ?";
+  private static final String GET_POST_BY_ID_AND_USER_QUERY =
       "SELECT * FROM posts WHERE id = ? AND \"user\" = ?";
   private static final String CREATE_NEW_POST_QUERY =
       "INSERT INTO posts (\"user\", title, content) VALUES (?, ?, ?) RETURNING id, title, content, \"user\"";
   private static final String UPDATE_POST_BY_ID_QUERY =
       "UPDATE posts SET title = ?, content = ? WHERE id = ?";
+  private static final String INCREMENT_POST_COMMENT_BY_ID_QUERY =
+      "UPDATE posts SET comments = comments + 1 WHERE id = ?";
+  private static final String DECREMENT_POST_COMMENT_BY_ID_QUERY =
+      "UPDATE posts SET comments = comments - 1 WHERE id = ?";
   private static final String DELETE_POST_BY_ID_QUERY =
       "DELETE FROM posts WHERE id = ?";
 
@@ -40,6 +46,19 @@ public class PostService {
       return ResponseVO.<List<Post>>builder().data(posts).build();
     } catch (Exception e) {
       throw new RuntimeException("Failed to get all posts", e);
+    }
+  }
+
+  public ResponseVO<Post> getPostById(int postId) {
+    try {
+      Post post = databaseService.executeQuery(GET_POST_BY_ID_QUERY, postId)
+          .stream()
+          .findFirst()
+          .map(this::toPost)
+          .orElse(null);
+      return ResponseVO.<Post>builder().data(post).build();
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to get post by id", e);
     }
   }
 
@@ -70,8 +89,8 @@ public class PostService {
           .build();
     }
     try {
-      if (databaseService.executeQuery(GET_POST_BY_ID_AND_USER, postId, request.getUser()).isEmpty()) {
-        return ResponseVO.<Boolean>builder().errors(Collections.singletonList("Data not found")).build();
+      if (databaseService.executeQuery(GET_POST_BY_ID_AND_USER_QUERY, postId, request.getUser()).isEmpty()) {
+        return ResponseVO.<Boolean>builder().errors(Collections.singletonList("Post data not found")).build();
       }
       databaseService.executeUpdate(UPDATE_POST_BY_ID_QUERY, request.getTitle(), request.getContent(), postId);
       return ResponseVO.<Boolean>builder().data(Boolean.TRUE).build();
@@ -81,14 +100,26 @@ public class PostService {
   }
 
   public ResponseVO<Boolean> deletePostById(int postId, String user) {
+    if (user == null) {
+      return ResponseVO.<Boolean>builder().errors(Collections.singletonList("The user field cannot be empty")).build();
+    }
     try {
-      if (databaseService.executeQuery(GET_POST_BY_ID_AND_USER, postId, user).isEmpty()) {
-        return ResponseVO.<Boolean>builder().errors(Collections.singletonList("Data not found")).build();
+      if (databaseService.executeQuery(GET_POST_BY_ID_AND_USER_QUERY, postId, user).isEmpty()) {
+        return ResponseVO.<Boolean>builder().errors(Collections.singletonList("Post data not found")).build();
       }
       databaseService.executeUpdate(DELETE_POST_BY_ID_QUERY, postId);
       return ResponseVO.<Boolean>builder().data(Boolean.TRUE).build();
     } catch (Exception e) {
       throw new RuntimeException("Failed to delete post");
+    }
+  }
+
+  public void modifyPostCommentById(int postId, boolean isIncrement) {
+    try {
+      String query = (isIncrement) ? INCREMENT_POST_COMMENT_BY_ID_QUERY : DECREMENT_POST_COMMENT_BY_ID_QUERY;
+      databaseService.executeUpdate(query, postId);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to modify post comment");
     }
   }
 
@@ -98,6 +129,7 @@ public class PostService {
         .title((String) mp.get("title"))
         .content((String) mp.get("content"))
         .user((String) mp.get("user"))
+        .comments((Integer) mp.get("comments"))
         .build();
   }
 
